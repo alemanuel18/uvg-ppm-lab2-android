61f1fbabbbecd4f00c9bb962f9aadb21b6491f34
alemanuel18 <jer24678@uvg.edu.gt>
2025-07-31 17:34:55 -0600

    SplashScreen crated

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,14 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.myapplication.ui.theme.MyApplicationTheme
+import kotlinx.coroutines.delay
+import androidx.compose.ui.graphics.Color
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,11 +31,19 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyApplicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                // Estado para controlar si se muestra la splash screen o el contenido principal
+                var showSplashScreen by remember { mutableStateOf(true) }
+
+                // Efecto para cambiar el estado después de un tiempo
+                LaunchedEffect(Unit) {
+                    delay(3000) // Muestra la splash screen por 3 segundos (3000 milisegundos)
+                    showSplashScreen = false
+                }
+
+                if (showSplashScreen) {
+                    SplashScreen()
+                } else {
+
                 }
             }
         }
@@ -31,17 +51,24 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun SplashScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize(), // Ocupa_todo el espacio disponible
+        verticalArrangement = Arrangement.Center, // Centra verticalmente
+        horizontalAlignment = Alignment.CenterHorizontally // Centra horizontalmente
+    ) {
+        Text(
+            text = "DragonStats",
+            fontSize = 50.sp, // Ajusta el tamaño del texto según necesitez
+            color = Color(red=13, green=113, blue=50)
+        )
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, name = "Splash Screen Preview")
 @Composable
-fun GreetingPreview() {
+fun SplashScreenPreview() {
     MyApplicationTheme {
-        Greeting("Android")
+        SplashScreen()
     }
 }
